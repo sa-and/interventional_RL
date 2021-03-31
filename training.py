6475a54404cb8fbe4bf853e4fe2e902a40ae0b84
Andreas Sauter <andreas.sauter@student.uni-tuebingen.de>
2021-03-31 10:38:36 +0200

    Implemented ACER training

--- training.py
+++ training.py
@@ -3,13 +3,12 @@ from Agents import DiscreteSwitchboardAgent, ContinuousSwitchboardAgent
 from stable_baselines.a2c import A2C
 from stable_baselines.common.policies import MlpLstmPolicy
 from stable_baselines.deepq.policies import MlpPolicy as dqnMlpPolicy
-from stable_baselines import DQN
+from stable_baselines import DQN, DDPG, ACER
 import random
 import stable_baselines.common.vec_env as venv
 import matplotlib.pyplot as plt
 from stable_baselines.ddpg.policies import MlpPolicy as ddpgMlpPolicy
-from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
-from stable_baselines import DDPG
+from stable_baselines.common.noise import NormalActionNoise
 from Agents import get_blank_switchboard_causal_graph
 
 
@@ -23,6 +22,40 @@ def create_switchboard_a2c_dynamic():
     agent = DiscreteSwitchboardAgent(5)
     a = Switchboard(agent, fixed_episode_length=False)
     return a
+
+
+def train_switchboard_acer(steps: int, workers: int = 8, fixed_length: bool = False):
+    if fixed_length:
+        switchboard = venv.DummyVecEnv([create_switchboard_a2c_fixed for i in range(workers)])
+    else:
+        switchboard = venv.DummyVecEnv([create_switchboard_a2c_dynamic for i in range(workers)])
+
+    # data collection phase
+    for i in range(500):
+        a = switchboard.envs[0].action_space.sample()
+        a = [a for i in range(workers)]
+        switchboard.step(a)
+    print('data collection phase done\n\n\n\n\n\n\n\n\n\n')
+
+    model = ACER(MlpLstmPolicy, switchboard,
+                 policy_kwargs={'net_arch': [50,
+                                             'lstm',
+                                             {'pi': [50],
+                                              'vf': [10]}],
+                                'n_lstm': 20},
+
+                 n_steps=5,
+                 n_cpu_tf_sess=8,
+                 replay_ratio=10,
+                 buffer_size=50000
+                 )
+
+    model.learn(steps)
+    title = 'ACER, discrete agnt, fixed = ' + str(fixed_length)
+    plt.title(title)
+    plt.plot(switchboard.envs[0].rewards)
+    plt.show()
+    return model, switchboard
 
 
 def train_switchboard_a2c(steps: int, workers: int = 8, fixed_length: bool = False):
@@ -110,7 +143,7 @@ def train_switchboard_ddpg(steps: int):
 
 #check = check_env(swtchbrd)
 
-model, board = train_switchboard_a2c(400000, fixed_length=False)
+model, board = train_switchboard_acer(400000, fixed_length=False)
 #model = DQN.load('models/exp3.zip', swtchbrd)
 
-model.save('models/exp16')
+model.save('models/exp17')
